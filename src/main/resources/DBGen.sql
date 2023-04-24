2c29a2762f9e614d56137edcdfbfb1d440fc606f
mhewb <mhe.bousquet@gmail.com>
2023-04-24 15:14:49 +0200

    fix(jsp): images path

--- src/main/resources/DBGen.sql
+++ src/main/resources/DBGen.sql
@@ -12,6 +12,14 @@ CREATE TABLE Users (
                        PRIMARY KEY (id)
 );
 
+
+CREATE TABLE Categories (
+                            id INT auto_increment,
+                            name VARCHAR(100),
+                            PRIMARY KEY (id)
+);
+
+
 CREATE TABLE Posts (
                        id INT auto_increment,
                        title VARCHAR(100),
@@ -19,9 +27,12 @@ CREATE TABLE Posts (
                        content TEXT,
 #                        createdAt VARCHAR(255),
                        imgURL TEXT,
+                       id_Categories INT,
+                       FOREIGN KEY (id_Categories) REFERENCES Categories(id),
                        PRIMARY KEY (id)
 );
 
 INSERT INTO Users(username, email, password, isAdmin) VALUES ('admin', 'admin@admin.io', 'admin', true);
-INSERT INTO Posts(title, author, content, imgURL) VALUES ('Test title', 'admin', 'this is a test content', '/images/img1.jpg')
+INSERT INTO Posts(title, author, content, imgURL) VALUES ('Test title', 'admin', 'this is a test content', 'resources/img/img1.jpg');
+INSERT INTO Posts(title, author, content, imgURL) VALUES ('Test title second', 'admin', 'Hello World!', 'resources/img/img1.jpg');
 # SELECT id, username, email, password FROM users WHERE username='admin';
